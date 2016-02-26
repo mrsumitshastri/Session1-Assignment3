21677c4fe52db37050472142afd89a6b09c373a5
Sumit Shastri <mrsumitshastri@gmail.com>
2016-02-26 23:19:24 +0530

    Commit
    Commit

---
+++ src/com/Session1/Assignment3/Session1Assignment3.java
@@ -0,0 +1,9 @@
+package com.Session1.Assignment3;
+
+public class Session1Assignment3 {
+public static void main(String[] args) {
+	String str = "sumit";
+	int a = str;
+	// a cnnot be store string value 
+}
+}
